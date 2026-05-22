f94dec81452c04aeff5ff1215907cc52e73a38e7
ajitmane1810 <ajitmane1810@gmail.com>
2026-05-22 13:29:25 +0530

    commit code for  HTML Web Page

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
-  // Navbar Scroll
-        const navbar = document.getElementById("navbar");
-        const glassNavbar = document.getElementById("glassNavbar");
+// Navbar Scroll
+const navbar = document.getElementById("navbar");
+const glassNavbar = document.getElementById("glassNavbar");
 
-        window.addEventListener("scroll", () => {
+window.addEventListener("scroll", () => {
 
-            if (window.scrollY > 80) {
+    if (window.scrollY > 80) {
 
-                glassNavbar.classList.add(
-                    "glass-scroll",
-                    "scale-[0.98]"
-                );
+        glassNavbar.classList.add(
+            "glass-scroll",
+            "scale-[0.98]"
+        );
 
-            } else {
+    } else {
 
-                glassNavbar.classList.remove(
-                    "glass-scroll",
-                    "scale-[0.98]"
-                );
+        glassNavbar.classList.remove(
+            "glass-scroll",
+            "scale-[0.98]"
+        );
 
-            }
+    }
 
-        });
+});
 
-        // Progress Bar
-        window.onscroll = function () {
+// Progress Bar
+window.onscroll = function () {
 
-            let winScroll =
-                document.body.scrollTop ||
-                document.documentElement.scrollTop;
+    let winScroll =
+        document.body.scrollTop ||
+        document.documentElement.scrollTop;
 
-            let height =
-                document.documentElement.scrollHeight -
-                document.documentElement.clientHeight;
+    let height =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
 
-            let scrolled =
-                (winScroll / height) * 100;
+    let scrolled =
+        (winScroll / height) * 100;
 
-            document.getElementById("progressBar").style.width =
-                scrolled + "%";
-        };
+    document.getElementById("progressBar").style.width =
+        scrolled + "%";
+};
 
 
 // MOBILE DRAWER
@@ -110,91 +110,91 @@ drawerLinks.forEach(link => {
 
 
 
-        // drawers menu
-        function navigateToScheme() {
-            Toaster.postMessage("navigateScheme");
-        }
+// drawers menu
+function navigateToScheme() {
+    Toaster.postMessage("navigateScheme");
+}
 
-        function navigateToCatalogues() {
-            Toaster.postMessage("navigateToCatalogues");
-        }
+function navigateToCatalogues() {
+    Toaster.postMessage("navigateToCatalogues");
+}
 
-        function navigateToQuickPurchase() {
-            Toaster.postMessage("navigateToQuickPurchase");
-        }
+function navigateToQuickPurchase() {
+    Toaster.postMessage("navigateToQuickPurchase");
+}
 
 
-        function navigateToBankDetails() {
-            Toaster.postMessage("navigateBankDetails");
-        }
+function navigateToBankDetails() {
+    Toaster.postMessage("navigateBankDetails");
+}
 
-        function navigateToKyc() {
-            Toaster.postMessage("navigateKyc");
-        }
+function navigateToKyc() {
+    Toaster.postMessage("navigateKyc");
+}
 
-        function navigateToRate() {
-            Toaster.postMessage("navigateRate");
-        }
+function navigateToRate() {
+    Toaster.postMessage("navigateRate");
+}
 
-        function navigateSchemeRecords() {
-            Toaster.postMessage("navigateSchemeRecords");
-        }
+function navigateSchemeRecords() {
+    Toaster.postMessage("navigateSchemeRecords");
+}
 
-        function navigateToCustomerCard() {
-            Toaster.postMessage("navigateCustomerCard");
-        }
+function navigateToCustomerCard() {
+    Toaster.postMessage("navigateCustomerCard");
+}
 
-        function navigateTransactionHistory() {
-            Toaster.postMessage("navigateTransactionHistory");
-        }
+function navigateTransactionHistory() {
+    Toaster.postMessage("navigateTransactionHistory");
+}
 
-        function navigateToMyOrder() {
-            Toaster.postMessage("navigateToMyOrder");
-        }
+function navigateToMyOrder() {
+    Toaster.postMessage("navigateToMyOrder");
+}
 
-        function navigateToUserProfile() {
-            Toaster.postMessage("navigateToUserProfile");
-        }
+function navigateToUserProfile() {
+    Toaster.postMessage("navigateToUserProfile");
+}
 
-        function navigateToDigitalGold() {
-            Toaster.postMessage("navigateToDigitalGold");
-        }
+function navigateToDigitalGold() {
+    Toaster.postMessage("navigateToDigitalGold");
+}
 
-        function navigateToPanchang() {
-            Toaster.postMessage("navigateToPanchang");
-        }
+function navigateToPanchang() {
+    Toaster.postMessage("navigateToPanchang");
+}
 
-        function navigateToFeedback() {
-            Toaster.postMessage("navigateToFeedback");
-        }
+function navigateToFeedback() {
+    Toaster.postMessage("navigateToFeedback");
+}
 
-        function navigateToSetting() {
-            Toaster.postMessage("navigateToSetting");
-        }
+function navigateToSetting() {
+    Toaster.postMessage("navigateToSetting");
+}
 
-        function setUserName(username) {
-            // Find the element by ID and set the text content
-            document.getElementById("user-name-display").innerText =
-                "Welcome, " + username;
-        }
-        function navigateToHomePage() {
-            Toaster.postMessage("navigateToHomePage");
-        }
+function setUserName(username) {
+    // Find the element by ID and set the text content
+    document.getElementById("user-name-display").innerText =
+        "Welcome, " + username;
+}
+function navigateToHomePage() {
+    Toaster.postMessage("navigateToHomePage");
+}
 
-        function navigateToARVirtualTryOn() {
-            Toaster.postMessage("navigateToARVirtualTryOn");
-        }
+function navigateToARVirtualTryOn() {
+    Toaster.postMessage("navigateToARVirtualTryOn");
+}
 
-        function toggleTabs() {
-            const extraTabs = document.querySelectorAll('[data-extra]');
-            const text = document.getElementById('toggleText');
-            const icon = document.getElementById('toggleIcon');
+function toggleTabs() {
+    const extraTabs = document.querySelectorAll('[data-extra]');
+    const text = document.getElementById('toggleText');
+    const icon = document.getElementById('toggleIcon');
 
-            let isHidden = [...extraTabs].some(tab => tab.classList.contains('hidden'));
-            extraTabs.forEach(tab => {
-                tab.classList.toggle('hidden', !isHidden);
-            });
+    let isHidden = [...extraTabs].some(tab => tab.classList.contains('hidden'));
+    extraTabs.forEach(tab => {
+        tab.classList.toggle('hidden', !isHidden);
+    });
 
-            text.innerText = isHidden ? 'Show Less' : 'Show More';
-            icon.classList.toggle('rotate-180', isHidden);
-        }
+    text.innerText = isHidden ? 'Show Less' : 'Show More';
+    icon.classList.toggle('rotate-180', isHidden);
+}
